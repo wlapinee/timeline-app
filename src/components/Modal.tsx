2186cda0ed190f744429f0a0d614b01c57d3c241
Watcharavit Lapinee <wlapinee@deloitte.com>
2026-03-22 23:06:24 +0700

    feat: edit overlay background on modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -25,7 +25,7 @@ export default function Modal({ open, onClose, title, children, maxWidth = 'max-
       onClick={onClose}
     >
       {/* Backdrop */}
-      <div className="absolute inset-0 bg-black/60 backdrop-blur-sm" />
+      <div className="fixed inset-0 bg-black/60 backdrop-blur-sm" />
 
       {/* Content */}
       <div
